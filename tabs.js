f9c0d69e36e661228efb7dd7efd9a18deec03348
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-06-26 22:38:45 +0300

    Add files via upload

--- tabs.js
+++ tabs.js
@@ -10,12 +10,14 @@ $("ul.tabs li").click(function () {
     $(".tab_accordion").removeClass("d_active");
     $(".tab_accordion[rel^='" + activeTab + "']").addClass("d_active");
 });
-$(".tab_accordion").click(function () {
-    $(".tab_content").hide();
-    var d_activeTab = $(this).attr("rel");
-    $("#" + d_activeTab).fadeIn();
-    $(".tab_accordion").removeClass("d_active");
-    $(this).addClass("d_active");
-    $("ul.tabs li").removeClass("active");
-    $("ul.tabs li[rel^='" + d_activeTab + "']").addClass("active");
+$(".dropdown").each((i,el) => { 
+    let list =  $(el).find('.tab_content2');
+    let img = $(el).find('.dropdown-img');
+    let dropdown = $(el).find('.dropdown-toggler');
+    dropdown.click(() => {
+        list.slideToggle('fast');
+        img.toggleClass("rotate-180");
+        dropdown.toggleClass("active");
+        list.toggleClass('hidden');
+    });
 });
